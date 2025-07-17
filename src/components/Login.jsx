4105f73f70df228423e98e1991ec4e69d24491f2
Juan Agudelo <verojuan9007@gmail.com>
2025-07-17 13:40:48 -0500

    login form logic done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [islogged, setIslogged] = useState(false);
+
   return (
-    <div className="min-h-screen flex justify-center items-center bg-black/40  ">
-      <div className=" w-[350px] max-w[200px] bg-black/80   p-12 flex flex-col justify-center items-center gap-6 rounded-lg">
-        <h1 className="text-2xl ">Login</h1>
+    <div className="min-h-screen flex justify-center items-center bg-black/10  backdrop-blur-xl ">
+      <div className=" w-[350px] max-w-[300px] bg-black/80   p-12 flex flex-col justify-center items-center gap-6 rounded-lg">
+        <h1 className="text-2xl ">{!islogged ? "Login" : "Sign up"}</h1>
         <form action="" className="flex flex-col gap-3 ">
-          <input
-            className="outline-0 border-0 border-b border-white bg-transparent "
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Name"
-          />
+          {islogged && (
+            <input
+              className="outline-0 border-0 border-b border-white bg-transparent "
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Name"
+            />
+          )}
           <input
             className="outline-0 border-0 border-b border-white bg-transparent "
             type="email"
@@ -27,17 +31,25 @@ const Login = () => {
             id="password"
             placeholder="Password"
           />
-          <input
-            className="outline-0 border-0 border-b border-white bg-transparent "
-            type="password"
-            name="confirmPassword"
-            id="confirmPassword"
-            placeholder="ConfirmPassword"
-          />
-          <button>Login</button>
-          <a className="underline s" href="">
-            Dont have an account?{" "}
-          </a>
+          {islogged && (
+            <input
+              className="outline-0 border-0 border-b border-white bg-transparent "
+              type="password"
+              name="confirmPassword"
+              id="confirmPassword"
+              placeholder="ConfirmPassword"
+            />
+          )}
+          <button type="submit" onClick={() => setIslogged(!islogged)}>
+            {!islogged ? "login" : "sign up "}
+          </button>
+          <button
+            className="underline s"
+            type="button"
+            onClick={() => setIslogged(!islogged)}
+          >
+            {!islogged ? " Dont have an account?" : "Already have an account"}
+          </button>
         </form>
       </div>
     </div>
