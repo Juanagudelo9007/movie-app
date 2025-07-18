22ff6c6cd25bf749714c8bedf179634367f263d0
Juan Agudelo <verojuan9007@gmail.com>
2025-07-18 09:27:41 -0500

    error while login added

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 1. fix the router - the navigation should be only between home and favorites. private routes / nested routes
 2. work on auth.
 3. test menu mobile routes.
+
+---
+
+auth done, register and login good to go - now lets work on errors, some design and routes
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,16 +10,32 @@ import {
 const Login = () => {
   const [islogged, setIslogged] = useState(false);
   const [register, setRegister] = useState(false);
+  const [error, setError] = useState("");
 
   const auth = getAuth(app);
+
+  {
+    /* Handler submit */
+  }
 
   const handlerSubmit = async (e) => {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
     const name = e.target.name ? e.target.name.value : "";
+    const confirmPassword = e.target.confirmPassword?.value || "";
 
     console.log(name, password, email);
+
+    {
+      /* Password error */
+    }
+
+    if (register && password !== confirmPassword) {
+      setError("Passwords do not match");
+      return;
+    }
+
     try {
       if (register) {
         const credentials = await createUserWithEmailAndPassword(
@@ -38,18 +54,50 @@ const Login = () => {
         console.log("user logged in:", userSignIn);
       }
     } catch (error) {
-      console.log(error);
+      const message = errorMessageFirebase(error.code);
+      setError(message);
+    }
+  };
+
+  {
+    /* Handler firebase error */
+  }
+
+  const errorMessageFirebase = (code) => {
+    switch (code) {
+      case "auth/user-not-found":
+      case "auth/wrong-password":
+        return "User or Password invalid";
+      case "auth/email-already-in-use":
+        return "Email already use";
+      case "auth/invalid-email":
+        return "Invalid Email";
+      case "auth/weak-password":
+        return "Weak password, password needs to have atleast 6 characters";
+      default:
+        return "An error happened please try again";
     }
   };
 
   return (
-    <div className="min-h-screen bg-cover bg-center flex justify-center items-center bg-[url('bg.jpg')]">
-      <div className="w-[300px] sm:w-[350px] bg-black/80 backdrop-blur-sm   p-12 flex flex-col justify-center items-center gap-6 rounded-lg">
+    <div className="relative min-h-screen bg-cover bg-center flex justify-center items-center bg-[url('bg.jpg')]">
+      {error && (
+        <div className="absolute z-30  bg-black/40 backdrop-blur-xl w-[250px] h-[120px] flex  flex-col gap-3 justify-center items-center rounded-sm">
+          <p className="text-white text-xs">{error}</p>
+          <button
+            onClick={() => setError("")}
+            className="text-xs text-white  bg-red-600 px-2  rounded-sm cursor-pointer"
+          >
+            ok
+          </button>
+        </div>
+      )}
+      <div className="w-[250px] sm:w-[300px] bg-black/80 backdrop-blur-sm   p-6 flex flex-col justify-center items-center gap-6 rounded-lg">
         <h1 className="text-2xl ">{!islogged ? "Login" : "Sign up"}</h1>
         <form
           onSubmit={handlerSubmit}
           action=""
-          className="flex flex-col gap-3 "
+          className="flex flex-col gap-2 "
         >
           {islogged && (
             <input
@@ -83,13 +131,19 @@ const Login = () => {
               placeholder="Confirm Password"
             />
           )}
-          <button type="submit">{!islogged ? "login" : "sign up "}</button>
           <button
-            className="underline s"
+            className="font-bold px-4-py2 bg-red-500 rounded-sm cursor-pointer"
+            type="submit"
+          >
+            {!islogged ? "Login" : "Sign up "}
+          </button>
+          <button
+            className="underline cursor-pointer"
             type="button"
             onClick={() => {
               setIslogged(!islogged);
               setRegister(!register);
+              setError("");
             }}
           >
             {!islogged ? " Dont have an account?" : "Already have an account?"}
