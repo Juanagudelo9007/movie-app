cb911d38fb1019a1ffc22dc017cb4eaf6e71f21e
Juan Agudelo <verojuan9007@gmail.com>
2025-07-30 15:33:00 -0500

    size of small card fixed

--- src/components/SmallCard.jsx
+++ src/components/SmallCard.jsx
@@ -6,15 +6,14 @@ const SmallCard = ({ movie }) => {
     : "https://via.placeholder.com/100x150?text=No+Image";
 
   return (
-    <div className="flex flex-col w-[80px] cursor-pointer ">
-      <img
-        src={posterUrl}
-        className="w-full aspect-[2/3] object-cover rounded-md"
-        alt={movie.title || "Sin título"}
-      />
-      <p className="text-xs text-center mt-1 truncate w-full">
-        {movie.title || "Sin título"}
-      </p>
+    <div className=" bg-transparent p-1">
+      <div className="w-[40px]  sm:w-[60px] md:w-[80px] aspect-[2/3] rounded-md shadow-[0_0_12px_rgba(154,99,248,0.6)] overflow-hidden flex flex-shrink-0">
+        <img
+          src={posterUrl}
+          className="w-full aspect-[2/3] object-cover"
+          alt={movie.title || "No title"}
+        />
+      </div>
     </div>
   );
 };
