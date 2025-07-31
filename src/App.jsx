a9dc66195a065a45be38bd79a37d6db3acf1e8a6
Juan Agudelo <verojuan9007@gmail.com>
2025-07-30 21:25:55 -0500

    animation between login and home done

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,25 @@ import Login from "./components/Login";
 import { LoginContext } from "./Context/UserLogin";
 import { AnimatePresence, motion } from "framer-motion";
 import { useContext } from "react";
-import {  useEffect } from "react";
+import { useEffect } from "react";
 import { app } from "./firebase/firebase";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import PrivateRoutes from "./routes/PrivateRoutes";
 
 const App = () => {
-  const { user,setUser } = useContext(LoginContext);
-    useEffect(() => {
-      const auth = getAuth(app);
-      const unsubscribe = onAuthStateChanged(auth, (userFirebase) => {
-        if (userFirebase) {
-          setUser(userFirebase);
-        } else {
-          setUser(false);
-        }
-      });
+  const { user, setUser } = useContext(LoginContext);
+  useEffect(() => {
+    const auth = getAuth(app);
+    const unsubscribe = onAuthStateChanged(auth, (userFirebase) => {
+      if (userFirebase) {
+        setUser(userFirebase);
+      } else {
+        setUser(false);
+      }
+    });
 
-      return () => unsubscribe();
-    }, []);
-  
+    return () => unsubscribe();
+  }, []);
 
   return (
     <AnimatePresence mode="wait" initial={false}>
@@ -31,8 +30,8 @@ const App = () => {
           key="private"
           initial={{ opacity: 0, scale: 1 }}
           animate={{ opacity: 1, scale: 1 }}
-          exit={{ opacity: 0, scale: 0 }}
-          transition={{ duration: 0.6, ease: "easeInOut" }}
+          exit={{ opacity: 0, scale: 0.9 }}
+          transition={{ duration: 0.5, ease: "easeInOut" }}
           style={{ transformOrigin: "center" }}
         >
           <PrivateRoutes />
@@ -42,8 +41,8 @@ const App = () => {
           key="login"
           initial={{ opacity: 0, scale: 1 }}
           animate={{ opacity: 1, scale: 1 }}
-          exit={{ opacity: 0, scale: 0 }}
-          transition={{ duration: 0.6, ease: "easeInOut" }}
+          exit={{ opacity: 0, scale: 0.95 }}
+          transition={{ duration: 0.5, ease: "easeInOut" }}
           style={{ transformOrigin: "center" }}
         >
           <Login />
@@ -54,6 +53,3 @@ const App = () => {
 };
 
 export default App;
-
-
- 
